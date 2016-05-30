e7eeb1c08dc006af84a7e992dc5bffd13337d3d1
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-05-30 11:11:46 +0300

    Final result

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+require('./bower_components/vaadin-charts/react');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MyApp = React.createClass({
+  getInitialState: function() {
+    return {
+      data: [],
+      inputNumber: ''
+    };
+  },
+  pushNumber: function() {
+    if (!isNaN(parseInt(this.state.inputNumber))) {
+      var newData = this.state.data;
+      newData.push(parseInt(this.state.inputNumber));
+      this.setState({data: newData});
+      this.setState({inputNumber: ''});
+    }
+  },
+  handleinputNumberChange: function(event) {
+    this.setState({inputNumber: event.target.value});
+  },
   render: function() {
     return (
-      <h1>Hello world</h1>
+      <div>
+      <input type="number" placeholder="number" value={this.state.inputNumber} onChange={this.handleinputNumberChange}/><button onClick={this.pushNumber}>Push that number</button>
+        <vaadin-line-chart>
+          <chart-title>Fibonacci</chart-title>
+          <x-axis>
+            <categories>January, February, March, April, May, June, July</categories>
+          </x-axis>
+          <data-series>
+            <data>65, 59, 80, 81, 56, 55, 40</data>
+          </data-series>
+          <data-series>
+            <data>28, 48, 40, 19, 86, 27, 90</data>
+          </data-series>
+          <data-series>
+            <data>{this.state.data.map(JSON.stringify).join(',')}</data>
+          </data-series>
+        </vaadin-line-chart>
+      </div>
     )
   }
 });
